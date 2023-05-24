214cbe7282081dc86e7e2884eb9af5e38b45ca0f
jcolpitt <jack.colpitt@crookcountyor.gov>
2023-05-24 15:58:20 -0700

    split search added

--- pats/pvforloop.py
+++ pats/pvforloop.py
@@ -26,32 +26,42 @@ def search_account(value):
 
 def tableSearchResults(value): # this search form needs error redirecting, or failed response built into the page
 
+    split_value = value.split()
+    print(split_value)
+
     propSearch_url = "https://geo.co.crook.or.us/server/rest/services/publicApp/Pats_Tables/MapServer/19/query"
     propTable_url = "https://geo.co.crook.or.us/server/rest/services/publicApp/Pats_Tables/MapServer/11/query"
     out_fields = "*"
     return_geometry = "false"
     f = "pjson"
-    where_clause = search_all(value)
-    url = f"{propSearch_url}?where={where_clause}&outFields={out_fields}&returnGeometry={return_geometry}&f={f}"
 
-    response = requests.get(url)
-    jsonResponse = response.json()
+    jr_list = []
+    for value in split_value:
+        where_clause = search_all(value)
+        url = f"{propSearch_url}?where={where_clause}&outFields={out_fields}&returnGeometry={return_geometry}&f={f}"
 
+        response = requests.get(url)
+        jsonResponse = response.json()
+        jr_list.append(jsonResponse)
+
+    print(jr_list)
     # if jsonResponse['features'] == []:
     #     print("I dont think there is anything here.")
 
     df_list = []
-    for element in jsonResponse['features']:
-        accounts = element['attributes']['account_id']
-        where_clause = search_account(accounts)
-        table_url = f"{propTable_url}?where={where_clause}&outFields={out_fields}&returnGeometry={return_geometry}&f={f}"
+    for dicts in jr_list:
 
-        responseTable = requests.get(table_url)
-        jsonResponseTable = responseTable.json()
+        for element in dicts['features']:
+            accounts = element['attributes']['account_id']
+            where_clause = search_account(accounts)
+            table_url = f"{propTable_url}?where={where_clause}&outFields={out_fields}&returnGeometry={return_geometry}&f={f}"
 
-        for elem in jsonResponseTable['features']:
-            root = Root.from_dict(elem)
-            df_list.append(root.attributes)
+            responseTable = requests.get(table_url)
+            jsonResponseTable = responseTable.json()
+
+            for elem in jsonResponseTable['features']:
+                root = Root.from_dict(elem)
+                df_list.append(root.attributes)
 
     df = pd.DataFrame(df_list)
 
--- pats/views.py
+++ pats/views.py
@@ -28,40 +28,47 @@ def mapPage(request):
 
 def tableSearchResults(request, value): # this search form needs error redirecting, or failed response built into the page
 
+    split_value = value.split()
+    #print(split_value)
+
     propSearch_url = "https://geo.co.crook.or.us/server/rest/services/publicApp/Pats_Tables/MapServer/19/query"
     propTable_url = "https://geo.co.crook.or.us/server/rest/services/publicApp/Pats_Tables/MapServer/11/query"
     out_fields = "*"
     return_geometry = "false"
     f = "pjson"
-    where_clause = search_all(value)
-    url = f"{propSearch_url}?where={where_clause}&outFields={out_fields}&returnGeometry={return_geometry}&f={f}"
 
-    response = requests.get(url)
-    jsonResponse = response.json()
+    jr_list = []
+    for value in split_value:
+        where_clause = search_all(value)
+        url = f"{propSearch_url}?where={where_clause}&outFields={out_fields}&returnGeometry={return_geometry}&f={f}"
 
-    if jsonResponse['features'] == []:
-        print("I dont think there is anything here.")
+        response = requests.get(url)
+        jsonResponse = response.json()
+        jr_list.append(jsonResponse)
+
+    #print(jr_list)
+    # if jsonResponse['features'] == []:
+    #     print("I dont think there is anything here.")
 
     df_list = []
-    for element in jsonResponse['features']:
-        accounts = element['attributes']['account_id']
-        where_clause = search_account(accounts)
-        table_url = f"{propTable_url}?where={where_clause}&outFields={out_fields}&returnGeometry={return_geometry}&f={f}"
+    for dicts in jr_list:
 
-        responseTable = requests.get(table_url)
-        jsonResponseTable = responseTable.json()
+        for element in dicts['features']:
+            accounts = element['attributes']['account_id']
+            where_clause = search_account(accounts)
+            table_url = f"{propTable_url}?where={where_clause}&outFields={out_fields}&returnGeometry={return_geometry}&f={f}"
 
-        for elem in jsonResponseTable['features']:
-            root = Root.from_dict(elem)
-            df_list.append(root.attributes)
+            responseTable = requests.get(table_url)
+            jsonResponseTable = responseTable.json()
+
+            for elem in jsonResponseTable['features']:
+                root = Root.from_dict(elem)
+                df_list.append(root.attributes)
 
     df = pd.DataFrame(df_list)
 
     json_records = df.reset_index().to_json(orient='records')
     data = json.loads(json_records)
-
-    html_table = df.to_html(classes='table table-striped', index=False)
-    context = {'html_table': html_table, 'd': data}
 
     html_table = df.to_html(classes='table table-striped', index=False)
     context = {'html_table': html_table, 'd': data}
