fbe315fd5c8022dfd93221a51f910eccb8b10092
jcolpitt <jack.colpitt@crookcountyor.gov>
2023-06-15 14:56:26 -0700

    land and structures added

--- pats/tests.py
+++ pats/tests.py
@@ -8,13 +8,21 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
-def account_query(account):
+def landandstructures(account):
     prop_url = "https://geo.co.crook.or.us/server/rest/services/publicApp/Pats_Tables/MapServer/11/query"
 
     # Define the query parameters
     params = {
         "where": f"account_id='{account}'",  # Retrieve all records
         "outFields": "*",  # Specify the fields to include in the response
+        "returnGeometry": False,  # Exclude geometry information
+        "f": "json"  # Specify the response format as JSON
+    }
+
+    las_params = {
+        "where": f"account_id='{account}'",  # Retrieve all records
+        "outFields": "account_id, description, stat_class, year_built, sqft",
+        # Specify the fields to include in the response
         "returnGeometry": False,  # Exclude geometry information
         "f": "json"  # Specify the response format as JSON
     }
@@ -29,26 +37,29 @@ def account_query(account):
         mt_find = mt[:mt.find('-', mt.find('-') + 1)]
         maptaxlot.append(mt_find.replace('-', ''))
 
-    zoning_url = "https://geo.co.crook.or.us/server/rest/services/publicApp/Pats_Tables/MapServer/18/query"
+    las_url = "https://geo.co.crook.or.us/server/rest/services/publicApp/Pats_Tables/MapServer/8/query"
 
-    # Define the query parameters
-    zoning_params = {
-        "where": f"maptaxlot='{maptaxlot[0]}'",  # Retrieve all records
-        "outFields": "*",  # Specify the fields to include in the response
-        "returnGeometry": False,  # Exclude geometry information
-        "f": "json"  # Specify the response format as JSON
-    }
+    las_response = requests.get(las_url, params=las_params)
+    las_data = las_response.json()
 
-    zoning_response = requests.get(zoning_url, params=zoning_params)
-    zoning_data = zoning_response.json()
+    land_char_url = "https://geo.co.crook.or.us/server/rest/services/publicApp/Pats_Tables/MapServer/9/query"
 
-    for zones in zoning_data['features']:
-        (zone := zones['attributes']['zone'])
-        (zone_desc := zones['attributes']['zone_desc'])
-        (zone_link := zones['attributes']['zone_link'])
+    land_char_response = requests.get(land_char_url, params=params)
+    land_char_data = land_char_response.json()
+    print(land_char_data)
 
-    print(prop_data)
-    #print(zone)
-    #context = {'data': prop_data, 'maptaxlot': maptaxlot, 'zone': zone, 'zone_desc': zone_desc, 'zone_link': zone_link}
 
-account_query(80000)
+    # dfLandCharList = []
+    # for i in land_char_data['features']:
+    #     print(i)
+    #     dfLandCharList.append(i['attributes'])
+    #
+    # dflcl = pd.DataFrame(dfLandCharList,
+    #                      columns=['land_description', 'decimal_acres', 'land_classification'])
+    # dflcl.columns = ['Land Description', 'Acres', 'Land Classification']
+    # dfLandCharTable = dflcl.sort_values(by='Acres', ascending=False)
+    #html_lcl_table = dflcl.to_html(classes='table table-dark', table_id='las_table', index=False)
+
+
+
+landandstructures(426)
